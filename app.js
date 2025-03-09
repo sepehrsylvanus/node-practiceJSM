0ca1a1906d55245c0561e732fa2590972b35b03a
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2025-03-09 21:43:44 +0300

    MongoDB

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ import express from "express";
 
 const app = express();
 import { PORT } from "./config/env.js";
+
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription tracker API!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Subscription tracker API listening on port ${PORT}!`);
+
+  await connectToDatabase();
 });
 
 export default app;
