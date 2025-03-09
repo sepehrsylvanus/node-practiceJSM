8a39484542326a5524bdc3508ca0440105a8b277
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2025-03-09 23:31:42 +0300

    authentication

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 import express from "express";
-
-const app = express();
 import { PORT } from "./config/env.js";
 
 import userRouter from "./routes/user.routes.js";
@@ -8,6 +6,13 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
