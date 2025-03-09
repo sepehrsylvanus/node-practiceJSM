f2344d7be433ea5316506fc3985498d7f5a46ef3
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2025-03-09 20:44:22 +0300

    config

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("server running");
+import express from "express";
+
+const app = express();
+import { PORT } from "./config/env.js";
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription tracker API!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Subscription tracker API listening on port ${PORT}!`);
+});
+
+export default app;
